8ed6f4b31fdf77e5c8ed9337119aa94e9682320d
Charles <chazylites@gmail.com>
2024-09-25 16:28:54 +0100

    Started the testimonial section

--- src/constants/index.js
+++ src/constants/index.js
@@ -154,27 +154,27 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        "I thought it was impossible to make a website as beautiful as our product, but Charles proved me wrong.",
+      name: "Pamphile MKP",
+      designation: "TRCN",
+      company: "Chill Academy",
+      image: "https://avatars.githubusercontent.com/u/98436409?v=4",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        "I've never met a web developer who truly cares about their clients' success like Charles does.",
+      name: "Basit Ali",
+      designation: "PKSTN",
+      company: "DRC 145",
+      image: "https://avatars.githubusercontent.com/u/63063973?v=4",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+        "After Charles optimized our website, our traffic increased by 50%. We can't thank him enough!",
+      name: "Gabriel Aghogho",
+      designation: "TRCN",
+      company: "Ciddi Mutfak",
+      image: "https://pbs.twimg.com/media/FqJ8a-CXgAUiUNF.jpg",
     },
   ];
   
