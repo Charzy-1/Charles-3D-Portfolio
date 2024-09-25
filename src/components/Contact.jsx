c6963492571abf6785dc1c0cfc34d8241d9e043f
Charles <chazylites@gmail.com>
2024-09-25 17:23:34 +0100

    pushed some codes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,18 @@ const Contact = () => {
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className="styles.sectionHeadText">Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form 
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 fles flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input type="text" name="name" value={{form.name}} onChange={handleChange} placeholder="Enter your name" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium "/>
+          </label>
+        </form>
       </motion.div>
       
     </div>
